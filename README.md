ac878d6afcd3974b18790755087263ebc63e868d
Anthony Ferrara <ircmaxell@gmail.com>
2013-02-13 12:39:19 -0500

    Initial commit of random support

---
+++ README.md
@@ -0,0 +1,6 @@
+RandomLib
+=========
+
+A library for generating random numbers and strings of various strengths.
+
+This library is useful in security contexts.
---
+++ test/Unit/RandomLib/Source/MicroTimeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RandomLib\Source;
+
+use SecurityLib\Strength;
+
+
+
+class MicroTimeTest extends \PHPUnit_Framework_TestCase {
+
+    public static function provideGenerate() {
+        $data = array();
+        for ($i = 0; $i < 100; $i += 5) {
+            $not = $i > 0 ? str_repeat(chr(0), $i) : chr(0);
+            $data[] = array($i, $not);
+        }
+        return $data;
+    }
+
+    /**
+     */
+    public function testGetStrength() {
+        $strength = new Strength(Strength::VERYLOW);
+        $actual = MicroTime::getStrength();
+        $this->assertEquals($actual, $strength);
+    }
+
+    /**
+     * @dataProvider provideGenerate
+     */
+    public function testGenerate($length, $not) {
+        $rand = new MicroTime;
+        $stub = $rand->generate($length);
+        $this->assertEquals($length, strlen($stub));
+        $this->assertNotEquals($not, $stub);
+    }
+
+}
